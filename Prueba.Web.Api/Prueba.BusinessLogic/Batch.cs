74ea695facfd57c80b19a3dd0b2ab680a6bca8ff
FEDE-VIRTUALBOX\fede <federicobouchaton@hotmail.com>
2017-09-23 15:20:43 -0300

    Fix TestCreateBatchWithStatus to succeed

--- Prueba.Web.Api/Prueba.BusinessLogic/Batch.cs
+++ Prueba.Web.Api/Prueba.BusinessLogic/Batch.cs
@@ -10,5 +10,6 @@ namespace Prueba.BusinessLogic
     {
         public string Description { get; set; }
         public string Name { get; set; }
+        public string Status { get; set; }
     }
 }
