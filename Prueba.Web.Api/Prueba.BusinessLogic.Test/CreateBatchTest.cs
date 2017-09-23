815318501aa19fcae5b4a8d9c195bc6925b359d2
FEDE-VIRTUALBOX\fede <federicobouchaton@hotmail.com>
2017-09-23 14:52:32 -0300

    Create test class CreateBatchTest

---
+++ Prueba.Web.Api/Prueba.BusinessLogic.Test/CreateBatchTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prueba.BusinessLogic.Test
+{
+    [TestClass]
+    public class CreateBatchTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
