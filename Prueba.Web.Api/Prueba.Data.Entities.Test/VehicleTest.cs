f15a589d6c81a784c5635269bab603381fbc6eea
npontet <nicopontet@gmail.com>
2017-09-24 17:25:45 -0300

    Red CreateVehicleTest

---
+++ Prueba.Web.Api/Prueba.Data.Entities.Test/VehicleTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prueba.Data.Entities.Test
+{
+    [TestClass]
+    public class VehicleTest
+    {
+        private string[] VehicleType = { "Auto", "Camión", "SUV", "VAN", "Minivan" };
+        [TestMethod]
+        public void CreateVehicleTest()
+        {
+             Vehicle vehicle = new Vehicle();
+
+             vehicle.ID = "1234567";
+             vehicle.Brand = "Peugeot";
+             vehicle.Model = "306";
+             vehicle.Year = "2010";
+             vehicle.Color = "Blue";
+             vehicle.VIN = "123";
+             vehicle.VehicleType = GetVehicleType(1);
+
+            Assert.AreEqual(vehicle.ID, "1234567");
+            Assert.AreEqual(vehicle.Brand, "1234567");
+            Assert.AreEqual(vehicle.Model, "1234567");
+            Assert.AreEqual(vehicle.Year, "1234567");
+            Assert.AreEqual(vehicle.VIN, "1234567");
+            Assert.AreEqual(vehicle.VehicleType, "Camion");
+
+        }
+
+        private object GetVehicleType(int n)
+        {
+            return VehicleType[n];
+        }
+    }
+}
